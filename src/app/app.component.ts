c667bf092e1741f9220e3ec541d8db02c98ab658
scriabin57 <scriabin57@gmail.com>
2025-04-17 00:44:57 +0000

    updated main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-root',
-  imports: [[RouterOutlet],[MatMenuModule],[MatIconModule]],
+  imports: [
+    RouterOutlet,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
+
+
 export class AppComponent {
-  title = 'Ed Skoviak Computing';
+  title = 'Our Philosophy';
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = this.positionOptions[1];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
