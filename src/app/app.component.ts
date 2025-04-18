adb87acb0fe8eac62b873cb6bb01713818ee921f
scriabin57 <scriabin57@gmail.com>
2025-04-18 01:42:19 +0000

    added navigation and business component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,21 +11,30 @@ import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatTooltipModule
-
+    MatTooltipModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
-
 export class AppComponent {
   title = 'Our Philosophy';
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = this.positionOptions[1];
+  
+  constructor(private router: Router) {}
+
+  onClick(event: Event, page: string) {
+    if (page == 'Business') {
+      console.log('Routing to Business page');
+      this.router.navigate(['Business']);  
+    }  
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BusinessComponent } from './business/business.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: 'Home', pathMatch: 'full' },
     { path: 'Home', component: HomeComponent },
+    { path: 'Business', component: BusinessComponent},
 
 ];
