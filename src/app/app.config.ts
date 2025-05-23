23b1e7c670eff39d30fe65b30fe3044274bf4f64
scriabin57 <scriabin57@gmail.com>
2025-05-23 18:24:39 +0000

    Added email and tidied up a bit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2321739814.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3188332389.
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()]
 };
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+interface MsgData {
+  name: string | null;
+  email: string | null;
+  message: string | null;
+}
+
 
 @Component({
   selector: 'app-contact-us',
@@ -10,24 +18,59 @@ import { Router } from '@angular/router';
 })
 export class ContactUsComponent {
   title = "Contact Us"
+  
   nameControl = new FormControl('');
   emailControl = new FormControl('');
   messageControl = new FormControl('');
 
-  constructor(private router: Router) { }
+  httpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+  /*
+  httpParams = new HttpParams({
+    fromObject: {
+      name: this.nameControl.value,
+      email: this.emailControl.value,
+      message: this.messageControl.value,
+    }
+  */    
+  
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   onSubmit() {
-    console.log('Name: ' + this.nameControl.value);
-    console.log('Email: ' + this.emailControl.value);
-    console.log('Message: ' + this.messageControl.value);
-    this.nameControl.setValue('');
-    this.emailControl.setValue('');
-    this.messageControl.setValue('');
-    // TODO call smtp service
-    alert('Message sent');
+    //console.log('Name: ' + this.nameControl.value);
+    //console.log('Email: ' + this.emailControl.value);
+    //console.log('Message: ' + this.messageControl.value);
+    const msgData: MsgData = {
+      name: this.nameControl.value,
+      email: this.emailControl.value,
+      message: this.messageControl.value,
+    }
+    //console.log(msgData);
+    this.sendMessage(msgData);
+
+    //this.nameControl.setValue('');
+    //this.emailControl.setValue('');
+    //this.messageControl.setValue('');
+    
     this.router.navigate(['/home']);
     
 
   }
 
+  sendMessage(data: any) {
+    //console.log(body)
+
+    this.http.post('https://mail-sender-821892242376.us-south1.run.app',
+      data,
+      {
+        headers: this.httpHeaders,
+        responseType: 'text'
+      }).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')}
+    );
+  }
 }
