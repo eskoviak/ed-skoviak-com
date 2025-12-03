4cc469aa35857fe6d850d036105fd2f4af42b60a
Ed Skoviak <eskoviak@gmail.com>
2025-12-02 19:48:08 -0600

    Ready to releast to production 0.9

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-technology',
   imports: [],
   templateUrl: './technology.component.html',
-  styleUrl: './technology.component.css'
+  styleUrl: './technology.component.css',
 })
-export class TechnologyComponent {
-  title = "Technology Architecture"
+export class TechnologyComponent implements OnInit {
+  title = 'Technology Architecture';
+  html?: SafeHtml;
 
-  constructor() {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
+  ngOnInit() {
+    this.http
+      .get('assets/pages/esc_technology.html', { responseType: 'text' })
+      .subscribe((content) => {
+        this.html = this.sanitizer.bypassSecurityTrustHtml(content);
+      });
+  }
 }
