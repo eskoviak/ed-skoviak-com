6f3cbf384e3d3285c0026eae5d3a9fe25f6d8518
Ed Skoviak <eskoviak@gmail.com>
2026-01-12 19:28:41 -0600

    Moved HTML source back to the component files

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, httpResource } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
